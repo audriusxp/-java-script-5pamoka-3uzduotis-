7d4a469797987a18d1b50633d5500b7d3950c605
audriusxp <palm@inbox.lt>
2022-06-15 10:08:40 +0300

    atlikta

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function (){
+    const testinis = [15,20,17,30,25,18,9,8];
+    console.log(testinis);
+    kasAntrasElementas(testinis);
+
+
+}
+function kasAntrasElementas(masyvas){
+     { for (let i = 0; i < masyvas.length; i+=2) {
+        console.log(masyvas[i]); 
+         
+     }
+        
+        
+        
+    }
+}
